fd90f9b8aaec751b8fdbca9880ae86d7c02712f7
Kojiee <mac7barcela@gmail.com>
2024-01-17 21:25:40 +0800

    add email format validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    VALID_EMAIL_REGEX = \A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+     #validate the format of an email address in a model.   
+    validates :email, presence: true,
+    uniqueness: { case_sensitive: false },
+    length: { maximum: 105 },
+    format: { with: VALID_EMAIL_REGEX }
 end
